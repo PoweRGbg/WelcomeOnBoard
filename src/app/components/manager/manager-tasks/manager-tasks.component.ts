86be6c4ca97a28d12b5c03631bb4c38d62eaa466
Roumen Georgiev <roumen_georgiev@yahoo.com>
2025-09-22 12:15:23 +0300

    added TODO

--- src/app/components/manager/manager-tasks/manager-tasks.component.ts
+++ src/app/components/manager/manager-tasks/manager-tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
