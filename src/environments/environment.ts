1fe88d43cf09eab9f614d9a78ad880eea4f2b34d
Rumen Georgiev <roumen35@gmail.com>
2025-10-08 17:44:26 +0300

    back to local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     useMockBackend: false, // Set to true for local testing without API
     apiUrlLocal: 'http://localhost:3030',
     apiUrl: 'https://protocols.nightscout.bg/api'
